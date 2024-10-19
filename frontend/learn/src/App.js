71eeb97c6fc0b65e5c4477ccebdaaa98c11ab3c8
AtharvaManchalkar <atharvamanchalkar7@gmail.com>
2024-10-19 23:12:34 +0530

    home page grid layout updated.

---
+++ frontend/learn/src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+            <h3>Learning Platform</h3>
+      </header>
+      <br/>
+      <div className="box1">
+      <div className = "placard1">
+       <h4>
+        Sem 1
+       </h4>
+      </div>
+      <div className="emptyspace">
+
+      </div>
+      <div className = "placard1">
+       <h4>
+        Sem 2
+       </h4>
+      </div>
+      </div>
+      <br/>
+      <div className="box2">
+      <div className = "placard2">
+       <h4>
+        Sem 3
+       </h4>
+      </div>
+      <div className="emptyspace">
+        
+      </div>
+      <div className = "placard2">
+       <h4>
+        Sem 4
+       </h4>
+      </div>
+      </div>
+      <br/>
+      <div className="box1">
+      <div className = "placard1">
+       <h4>
+        Sem 5
+       </h4>
+      </div>
+      <div className="emptyspace">
+
+      </div>
+      <div className = "placard1">
+       <h4>
+        Sem 6
+       </h4>
+      </div>
+      </div>
+      <br/>
+      <div className="box1">
+      <div className = "placard1">
+       <h4>
+        Sem 7
+       </h4>
+      </div>
+      <div className="emptyspace">
+
+      </div>
+      <div className = "placard1">
+       <h4>
+        Sem 8
+       </h4>
+      </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
